405fc37fa533e6912f4f007b2301c800d8c591d6
Samuel Mueller <mueller.samu@gmail.com>
2014-11-01 15:45:39 +0100

    replace tabs with whitespaces

--- js/geometry/Line.js
+++ js/geometry/Line.js
@@ -1,11 +1,11 @@
 var Line = new Class({
-	
-	initialize: function(p1, p2) {
-	this.p1 = p1;
-	this.p2 = p2;		
-	},
-	
-	equals: function(line) {
-		return this.p1.equals(line.p1) && this.p2.equals(line.p2);
-	}
+  
+  initialize: function(p1, p2) {
+  this.p1 = p1;
+  this.p2 = p2;    
+  },
+  
+  equals: function(line) {
+    return this.p1.equals(line.p1) && this.p2.equals(line.p2);
+  }
 });
--- js/util/Utility.js
+++ js/util/Utility.js
@@ -1,17 +1,17 @@
 function multiDimensionalArray(rows, columns) { 
     var a = new Array(rows); 
     for (var i = 0; i < rows; i++) { 
-    	a[i] = new Array(columns); 
+      a[i] = new Array(columns); 
         for (var j = 0; j < columns; j++) { 
-        	a[i][j] = new Array(); 
+          a[i][j] = new Array(); 
         } 
     } 
     return(a); 
 } 
 
 function getMousePoint(mouseEvent) {
-	var mousePoint = new Point(0,0);
-	mousePoint.x = mouseEvent.offsetX;
-	mousePoint.y = mouseEvent.offsetY;
-	return mousePoint;
+  var mousePoint = new Point(0,0);
+  mousePoint.x = mouseEvent.offsetX;
+  mousePoint.y = mouseEvent.offsetY;
+  return mousePoint;
 }
