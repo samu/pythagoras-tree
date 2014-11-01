4fef0f2e0866b0cab97967b37fdc02ab342d19e1
Samuel Mueller <mueller.samu@gmail.com>
2014-11-01 15:42:39 +0100

    initial commit

---
+++ js/geometry/Polygon.js
@@ -0,0 +1,60 @@
+var Polygon = new Class({
+	
+	initialize: function(edges) {
+		this.edges = edges;	
+	},
+	
+	rotate: function(matrix) {
+		var newedges = new Array();
+		for (var i = 0; i < this.edges.length; i++) {
+			newedges[i] = this.edges[i].rotate(matrix);
+		}
+		return new Polygon(newedges);
+	},
+
+	rotateAt: function(matrix, point) {
+		return this.transform(new Array(-point.x, -point.y)).rotate(matrix).transform(new Array(point.x, point.y));
+	},
+
+	transform: function(vector) {
+		var newedges = new Array();
+		for (var i = 0; i < this.edges.length; i++) {
+			newedges[i] = this.edges[i].transform(vector);
+		}
+		return new Polygon(newedges);
+	},
+
+	transformTo: function(from, to) {
+		var newedges = new Array();
+		for (var i = 0; i < this.edges.length; i++) {
+			newedges[i] = this.edges[i].transformTo(from, to);
+		}
+		return new Polygon(newedges);
+	},
+
+	scale: function(vector) {
+		var newedges = new Array();
+		for (var i = 0; i < this.edges.length; i++) {
+			newedges[i] = this.edges[i].scale(vector);
+		}
+		return new Polygon(newedges);
+	},
+	
+	scaleCentered: function(vector) {
+		//var boundingBox = BasicMath.getBoundingBox(this);
+		//var center = new Point(boundingBox[0] + boundingBox[2] / 2, boundingBox[1] + boundingBox[3] / 2);
+		var center = BasicMath.calculateCentroid(this);
+		return this.transformTo(center, new Point(0,0)).scale(vector).transformTo(new Point(0,0), center);
+	},
+
+	draw: function(canvas) {
+		canvas.beginPath();
+		canvas.moveTo(this.edges[0].x, this.edges[0].y);
+		for (var i = 1; i < this.edges.length; i++) {
+			canvas.lineTo(this.edges[i].x, this.edges[i].y);
+		}
+		canvas.fill();
+		canvas.closePath();
+	}
+	
+});
